3c02b8db3ccd3bcd08c00acb5f8326c7e37c3ce8
JP Sio <sio.jeanpaul@gmail.com>
2020-03-03 07:29:50 -0800

    try test mongo

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect("mongodb://127.0.0.1:27017/node-boilerplate-api-test", {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
